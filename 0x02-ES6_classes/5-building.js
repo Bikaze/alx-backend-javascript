ed4d41f8490301397be15716e91a5429b10008db
Bikaze <clmntmugisha@gmail.com>
2024-06-27 19:11:30 +0200

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,12 +1,13 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    if (this.evacuationWarningMessage === undefined) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  evacuationWarningMessage() { //eslint-disable-line
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
