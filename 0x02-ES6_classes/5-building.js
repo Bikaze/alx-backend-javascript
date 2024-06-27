913871cba608edcb4617354941e484f9a6ce08a8
Bikaze <clmntmugisha@gmail.com>
2024-06-27 19:03:19 +0200

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,7 +3,7 @@ export default class Building {
     if (new.target === Building) {
       throw new Error('Cannot instantiate from Building, it is an abstract class');
     }
-    this._sqft = sqft;
+    this.sqft = sqft;
     if (this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
