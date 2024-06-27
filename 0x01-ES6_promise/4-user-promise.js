e073a952a0438a6329c89fa9ee4d926c9e4948cb
Bikaze <clmntmugisha@gmail.com>
2024-06-27 11:06:13 +0200

    Create 4-user-promise.js

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,5 @@
+export default function signUpUser(firstName, lastName) {
+  return new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
+    resolve({ firstName, lastName });
+  });
+}
