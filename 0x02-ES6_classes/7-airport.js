7c942600703c70d2a44fd84d88a8ea71706ad83a
Bikaze <clmntmugisha@gmail.com>
2024-06-28 10:07:09 +0200

    Create 7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,10 @@
+class Airport { //eslint-disable-line
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
