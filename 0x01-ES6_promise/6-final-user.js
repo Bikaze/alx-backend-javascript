1985ec1e3a8204d1b13ba2cd76e66d9b5be27552
Bikaze <clmntmugisha@gmail.com>
2024-06-28 12:10:14 +0200

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,16 @@
-import signUpUser from './4-user-promise.js'; // eslint-disable-line
+import signUpUser from './4-user-promise.js'; //eslint-disable-line
 import uploadPhoto from './5-photo-reject.js'; //eslint-disable-line
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
+
+  return results.map((result) => ({
+    status: result.status,
+    value: result.reason || result.value,
+  }));
 }
+
+export default handleProfileSignup;
