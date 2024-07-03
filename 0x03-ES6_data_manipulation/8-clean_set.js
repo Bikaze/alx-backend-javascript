f26a539e600a92324caa6ecfd2a7efaa3b5ae28a
Bikaze <clmntmugisha@gmail.com>
2024-07-03 09:26:38 +0200

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
+  if (!startString || typeof startString !== 'string') return '';
 
-  const result = [];
+  let result = '';
   for (const item of set) {
-    if (item.startsWith(startString)) {
-      result.push(item.slice(startString.length));
+    if (typeof item === 'string' && item.startsWith(startString)) {
+      result += `${item.slice(startString.length)}-`;
     }
   }
 
-  return result.join('-');
+  return result.slice(0, -1); // Remove the last '-' from the result string
 }
