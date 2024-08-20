62541a7be7a2c60f1914d555e23c8422dc8eb293
Bikaze <clmntmugisha@gmail.com>
2024-08-20 12:49:04 +0200

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,10 @@
 /**
- * prints message on the console
- * @param {String} message
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ *
  */
-
-function displayMessage (message) {
-  console.log(message);
-}
+const displayMessage = (msg) => {
+  console.log(msg);
+};
 
 module.exports = displayMessage;
